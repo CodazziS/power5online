00c53be69f2ea027f831fdc3f8d4f455d98ec08b
Stephane Codazzi <stephane.codazzi@gmail.com>
2018-02-12 21:01:59 +0100

    Update code

--- imports/api/boards.js
+++ imports/api/boards.js
@@ -19,232 +19,6 @@ if (Meteor.isServer) {
     });
 }
 
-Meteor.methods({
-
-    'boards.insert'(game) {
-        let opponentType = null;
-        let opponentId = null;
-        let opponentUsername = null;
-        const currentUser = getCurrentUser(game.guestId);
-
-        check(game.size, Number);
-        check(game.game, String);
-
-        if (game.size < 6 || game.size > 30) {
-            throw new Meteor.Error('bad-size');
-        }
-
-        if (game.opponent) {
-            let opponent = Meteor.users.findOne({username: { $regex: new RegExp("^" + game.opponent, "i") }});
-            if (opponent) {
-                opponentType = 'meteor';
-                opponentId = opponent._id;
-                opponentUsername = opponent.username;
-            } else {
-                throw new Meteor.Error('player-not-found');
-            }
-        }
-
-        return Boards.insert({
-            size: game.size,
-            game: game.game,
-            dots: dotsGeneration(game.size),
-            whiteIsNext: true,
-            creatorIsWhite: (Math.floor(Math.random() * Math.floor(2)) === 1),
-            authorType: currentUser.userType,
-            authorId: currentUser.userId,
-            authorUsername: currentUser.userName,
-            authorReplay: false,
-            opponentType: opponentType,
-            opponentId: opponentId,
-            opponentUsername: opponentUsername,
-            opponentReplay: false,
-            createdAt: new Date(),
-            end: false,
-            winnerIsAuthor: false,
-            draw: false,
-            replay_id: null
-        });
-    },
-
-    'boards.setOpponent'(gameId, guest) {
-        const currentUser = getCurrentUser(guest);
-
-        // let user_id = null;
-        // let user_name = null;
-        // let user_type = null;
-
-        check(gameId, String);
-        // if (Meteor.user()) {
-        //     user_id = Meteor.user()._id;
-        //     user_name = Meteor.user().username;
-        //     user_type = 'meteor';
-        // } else {
-        //     check(guest, String);
-        //     user_id = guest;
-        //     user_name = 'guest_' + user_id;
-        //     user_type = 'guest';
-        // }
-
-        Boards.update(gameId, {
-            $set: {
-                opponentType: currentUser.userType,
-                opponentId: currentUser.userId,
-                opponentUsername: currentUser.userName,
-            },
-        });
-    },
-
-    'boards.replay'(gameId, guest) {
-        const board = Boards.findOne(gameId);
-        // let user_id = null;
-        const currentUser = getCurrentUser(guest);
-
-        // if (Meteor.user()) {
-        //     user_id = Meteor.user()._id;
-        // } else {
-        //     check(guest, String);
-        //     user_id = guest;
-        // }
-        if (!board.end) {
-            return;
-        }
-        if ((board.authorId === currentUser.userId || board.authorId === guest) &&
-            !board.authorReplay && !board.replay_id) {
-            board.authorReplay = true;
-            Boards.update(gameId, {
-                $set: {
-                    authorReplay: true,
-                },
-            });
-        }
-        if ((board.opponentId === currentUser.userId || board.opponentId === guest) &&
-            !board.opponentReplay && !board.replay_id) {
-            board.opponentReplay = true;
-            Boards.update(gameId, {
-                $set: {
-                    opponentReplay: true,
-                },
-            });
-        }
-
-        if (board.authorReplay && board.opponentReplay && !board.replay_id) {
-            const newId = Boards.insert({
-                size: board.size,
-                game: board.game,
-                dots: dotsGeneration(board.size),
-                whiteIsNext: true,
-                creatorIsWhite: (Math.floor(Math.random() * Math.floor(2)) === 1),
-                authorType: board.authorType,
-                authorId: board.authorId,
-                authorUsername: board.authorUsername,
-                authorReplay: false,
-                opponentType: board.opponentType,
-                opponentId: board.opponentId,
-                opponentUsername: board.opponentUsername,
-                opponentReplay: false,
-                createdAt: new Date(),
-                end: false,
-                winnerIsAuthor: false,
-                draw: false,
-                replay_id: null
-            });
-            Boards.update(gameId, {
-                $set: {
-                    replay_id: newId,
-                },
-            });
-        }
-    },
-
-    'boards.replayAccepted'(gameId, guest) {
-        const board = Boards.findOne(gameId);
-        // let user_id = null;
-        const currentUser = getCurrentUser(guest);
-        //
-        // if (Meteor.user()) {
-        //     user_id = Meteor.user()._id;
-        // } else {
-        //     check(guest, String);
-        //     user_id = guest;
-        // }
-
-        if ((board.authorId === currentUser.userId || board.authorId === guest) &&
-            !board.authorReplay && board.replay_id) {
-            Boards.update(gameId, {
-                $set: {
-                    authorReplay: null,
-                },
-            });
-        }
-        if ((board.opponentId === currentUser.userId || board.opponentId === guest) &&
-            !board.opponentReplay && board.replay_id) {
-            Boards.update(gameId, {
-                $set: {
-                    opponentReplay: null,
-                },
-            });
-        }
-    },
-
-    'boards.addDot'(gameId, row, col, guest) {
-        // let user_id = null;
-        const board = Boards.findOne(gameId);
-        const whitePlayer = (board.creatorIsWhite === true) ? board.authorId :  board.opponentId;
-        const blackPlayer = (board.creatorIsWhite === false) ? board.authorId :  board.opponentId;
-        const currentUser = getCurrentUser(guest);
-        let dots = board.dots;
-
-        check(gameId, String);
-        // if (Meteor.user()) {
-        //     user_id = Meteor.user()._id;
-        // } else {
-        //     check(guest, String);
-        //     user_id = guest;
-        // }
-        if (!board.opponentId || dots[row][col].state || board.end) {
-            return;
-        }
-        if ((board.whiteIsNext && !(whitePlayer === currentUser.userId || whitePlayer === guest)) ||
-            (!board.whiteIsNext && !(blackPlayer === currentUser.userId || blackPlayer === guest))) {
-            return;
-        }
-
-        dots[row][col].state = board.whiteIsNext ? "white" : "black";
-
-        Boards.update(gameId, {
-            $set: {
-                dots: dots,
-                whiteIsNext: !board.whiteIsNext,
-                last: row + '-' + col
-            },
-        });
-
-        const checkWin = checkWinner(dots, board.size);
-        if (checkWin) {
-            let winner = checkWin.winner;
-            let winnerIsAuthor = null;
-            let winDots = checkWin.dots;
-
-            winnerIsAuthor = (winner === "white") ? (board.creatorIsWhite === true) : (board.creatorIsWhite === false);
-            dots[winDots[0][0]][winDots[0][1]].win = true;
-            dots[winDots[1][0]][winDots[1][1]].win = true;
-            dots[winDots[2][0]][winDots[2][1]].win = true;
-            dots[winDots[3][0]][winDots[3][1]].win = true;
-            dots[winDots[4][0]][winDots[4][1]].win = true;
-
-            Boards.update(gameId, {
-                $set: {
-                    dots: dots,
-                    end: true,
-                    winnerIsAuthor: winnerIsAuthor,
-                    draw: false
-                },
-            });
-        }
-    },
-});
-
 function dotsGeneration(size) {
     let dots = [];
     let rows = 0;
@@ -338,3 +112,194 @@ function checkWinner(dots, size) {
     }
     return false;
 }
+
+Meteor.methods({
+
+    'boards.insert'(game) {
+        let opponentType = null;
+        let opponentId = null;
+        let opponentUsername = null;
+        const currentUser = getCurrentUser(game.guestId);
+
+        check(game.size, Number);
+        check(game.game, String);
+
+        if (game.size < 6 || game.size > 30) {
+            throw new Meteor.Error('bad-size');
+        }
+
+        if (game.opponent) {
+            let opponentRegex = new RegExp(["^", game.opponent, "$"].join(""), "i");
+            let opponent = Meteor.users.findOne({username: { $regex: opponentRegex }});
+
+            if (opponent) {
+                opponentType = 'meteor';
+                opponentId = opponent._id;
+                opponentUsername = opponent.username;
+            } else {
+                throw new Meteor.Error('player-not-found');
+            }
+        }
+
+        return Boards.insert({
+            size: game.size,
+            game: game.game,
+            dots: dotsGeneration(game.size),
+            whiteIsNext: true,
+            creatorIsWhite: (Math.floor(Math.random() * Math.floor(2)) === 1),
+            authorType: currentUser.userType,
+            authorId: currentUser.userId,
+            authorUsername: currentUser.userName,
+            authorReplay: false,
+            opponentType: opponentType,
+            opponentId: opponentId,
+            opponentUsername: opponentUsername,
+            opponentReplay: false,
+            createdAt: new Date(),
+            end: false,
+            winnerIsAuthor: false,
+            draw: false,
+            replay_id: null
+        });
+    },
+
+    'boards.setOpponent'(gameId, guest) {
+        const currentUser = getCurrentUser(guest);
+
+        check(gameId, String);
+        Boards.update(gameId, {
+            $set: {
+                opponentType: currentUser.userType,
+                opponentId: currentUser.userId,
+                opponentUsername: currentUser.userName,
+            },
+        });
+    },
+
+    'boards.replay'(gameId, guest) {
+        const board = Boards.findOne(gameId);
+        const currentUser = getCurrentUser(guest);
+
+        if (!board.end) {
+            return;
+        }
+        if ((board.authorId === currentUser.userId || board.authorId === guest) &&
+            !board.authorReplay && !board.replay_id) {
+            board.authorReplay = true;
+            Boards.update(gameId, {
+                $set: {
+                    authorReplay: true,
+                },
+            });
+        }
+        if ((board.opponentId === currentUser.userId || board.opponentId === guest) &&
+            !board.opponentReplay && !board.replay_id) {
+            board.opponentReplay = true;
+            Boards.update(gameId, {
+                $set: {
+                    opponentReplay: true,
+                },
+            });
+        }
+
+        if (board.authorReplay && board.opponentReplay && !board.replay_id) {
+            const newId = Boards.insert({
+                size: board.size,
+                game: board.game,
+                dots: dotsGeneration(board.size),
+                whiteIsNext: true,
+                creatorIsWhite: (Math.floor(Math.random() * Math.floor(2)) === 1),
+                authorType: board.authorType,
+                authorId: board.authorId,
+                authorUsername: board.authorUsername,
+                authorReplay: false,
+                opponentType: board.opponentType,
+                opponentId: board.opponentId,
+                opponentUsername: board.opponentUsername,
+                opponentReplay: false,
+                createdAt: new Date(),
+                end: false,
+                winnerIsAuthor: false,
+                draw: false,
+                replay_id: null
+            });
+            Boards.update(gameId, {
+                $set: {
+                    replay_id: newId,
+                },
+            });
+        }
+    },
+
+    'boards.replayAccepted'(gameId, guest) {
+        const board = Boards.findOne(gameId);
+        const currentUser = getCurrentUser(guest);
+
+        if ((board.authorId === currentUser.userId || board.authorId === guest) &&
+            !board.authorReplay && board.replay_id) {
+            Boards.update(gameId, {
+                $set: {
+                    authorReplay: null,
+                },
+            });
+        }
+        if ((board.opponentId === currentUser.userId || board.opponentId === guest) &&
+            !board.opponentReplay && board.replay_id) {
+            Boards.update(gameId, {
+                $set: {
+                    opponentReplay: null,
+                },
+            });
+        }
+    },
+
+    'boards.addDot'(gameId, row, col, guest) {
+        const board = Boards.findOne(gameId);
+        const whitePlayer = (board.creatorIsWhite === true) ? board.authorId :  board.opponentId;
+        const blackPlayer = (board.creatorIsWhite === false) ? board.authorId :  board.opponentId;
+        const currentUser = getCurrentUser(guest);
+        let dots = board.dots;
+
+        check(gameId, String);
+        if (!board.opponentId || dots[row][col].state || board.end) {
+            return;
+        }
+        if ((board.whiteIsNext && !(whitePlayer === currentUser.userId || whitePlayer === guest)) ||
+            (!board.whiteIsNext && !(blackPlayer === currentUser.userId || blackPlayer === guest))) {
+            return;
+        }
+
+        dots[row][col].state = board.whiteIsNext ? "white" : "black";
+        Boards.update(gameId, {
+            $set: {
+                dots: dots,
+                whiteIsNext: !board.whiteIsNext,
+                last: row + '-' + col
+            },
+        });
+
+        const checkWin = checkWinner(dots, board.size);
+        if (checkWin) {
+            let winner = checkWin.winner;
+            let winnerIsAuthor = null;
+            let winDots = checkWin.dots;
+
+            winnerIsAuthor = (winner === "white") ? (board.creatorIsWhite === true) : (board.creatorIsWhite === false);
+            dots[winDots[0][0]][winDots[0][1]].win = true;
+            dots[winDots[1][0]][winDots[1][1]].win = true;
+            dots[winDots[2][0]][winDots[2][1]].win = true;
+            dots[winDots[3][0]][winDots[3][1]].win = true;
+            dots[winDots[4][0]][winDots[4][1]].win = true;
+
+            Boards.update(gameId, {
+                $set: {
+                    dots: dots,
+                    end: true,
+                    winnerIsAuthor: winnerIsAuthor,
+                    draw: false
+                },
+            });
+        }
+    },
+});
+
